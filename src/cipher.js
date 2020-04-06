6bec5a92913e4d8cfeb68a81a89ffe88fd9a299f
elainefigueiredo87 <elaine.figueiredo87@gmail.com>
2020-04-06 09:14:37 -0300

    atualizacao html

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,28 @@
-const cipher = {
+  const cipher = {
+    encode(deslocamento, texto){
+      Number(deslocamento)
+      texto = texto.toUpperCase()
+      let messageArray = []
+      let textRetorno = ""
+      for (let i=0; i<texto.length; i++)
+      {
+       messageArray [i] = (texto.charAt(i))
+        messageArray[i] = (((messageArray[i].charCodeAt(0) - 65) + deslocamento) % 26 + 65)
+        texotRetorno += String.fromCharCode(messageArray[i])
+        alert (textoRetorno)
+
+      }
+      return textoRetorno
+
+    }
+  }
+
+    //decode(){
+
+  
+
+  
+   
   // ...
-};
 
 export default cipher;
