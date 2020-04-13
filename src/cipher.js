d6d6a5c6cafafaa4fe8eff3cec32403783045c61
elainefigueiredo87 <elaine.figueiredo87@gmail.com>
2020-04-13 00:16:45 -0300

    cipher-ok

--- src/cipher.js
+++ src/cipher.js
@@ -1,54 +1,54 @@
-  const cipher = {
-    encode: function (offset,string){
-      
-      let texto = string.toUpperCase()
-      let results = "";
-      
+const cipher = {
+  encode: function (offset, string) {
+
+    let texto = string.toUpperCase()
+    let results = "";
 
 
-      for (let i=0; i<texto.length; i++)
-      { let pegarLetras = texto[i]; 
-        let num ;
-        if (pegarLetras.charCodeAt(0)>= 65 && (pegarLetras.charCodeAt(0) <=90)){
-          num=pegarLetras.charCodeAt(0) - 65;
-          results += String.fromCharCode (((num + offset) % 26) + 65);
 
-        }
-       
+    for (let i = 0; i < texto.length; i++) {
+      let pegarLetras = texto[i];
+      let num;
+      if (pegarLetras.charCodeAt(0) >= 65 && (pegarLetras.charCodeAt(0) <= 90)) {
+        num = pegarLetras.charCodeAt(0) - 65;
+        results += String.fromCharCode(((num + offset) % 26) + 65);
 
       }
-      return results
 
-    }  ,
 
-    decode: function(offset,string){
-      let texto = string.toUpperCase();
-      let results = "";
-      
-
-      for (let i=0; i<texto.length; i++)
-      { let pegarLetras = texto[i]; 
-        let num ;
-        if (pegarLetras.charCodeAt(0)>= 65 && (pegarLetras.charCodeAt(0) <=90)){
-          num=pegarLetras.charCodeAt(0) - 90;
-          results += String.fromCharCode (((num - offset) % 26) + 90);
-
-        }
-       
-
-      }
-      return results
     }
-  
-  
-  
-  
+    return results
+
+  },
+
+  decode: function (offset, string) {
+    let texto = string.toUpperCase();
+    let results = "";
+
+
+    for (let i = 0; i < texto.length; i++) {
+      let pegarLetras = texto[i];
+      let num;
+      if ((pegarLetras.charCodeAt(0) >= 65) && (pegarLetras.charCodeAt(0) <= 90)) {
+        num = pegarLetras.charCodeAt(0) - 90;
+        results += String.fromCharCode(((num - offset) % 26) + 90);
+
+      }
+
+
+    }
+    return results
   }
 
-  
 
-  
-   
- 
+
+
+}
+
+
+
+
+
+
 
 export default cipher;
